dcd00b81552ef656b481cdd6863b82bfcdcc6264
archie.cobbs <archie.cobbs@5b2090ce-6c64-96d5-a530-528bb67746b7>
2010-12-07 16:23:05 +0000

    - Update man page. - Fail on Mac OS.

--- wiki/manpage.sh
+++ wiki/manpage.sh
@@ -7,7 +7,11 @@ cat << "xxEOFxx"
 {{{
 xxEOFxx
 
-groff -r LL=100n -r LT=100n -Tlatin1 -man ../trunk/csvprintf.1 | sed -r -e 's/.\x08(.)/\1/g' -e 's/[[0-9]+m//g' 
+if [ `uname -s` = 'Darwin' ]; then
+    echo This doesn\'t work on Mac OS 1>&2
+    exit 1
+fi
+groff -r LL=100n -r LT=100n -Tlatin1 -man ../trunk/csvprintf.1 | sed "${SEDFLAG}" -e 's/.\x08(.)/\1/g' -e 's/[[0-9]+m//g' 
 
 cat << "xxEOFxx"
 }}}
