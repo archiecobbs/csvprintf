332c01f0028f1952542754a0ee66c9ee6d04842a
Archie L. Cobbs <archie.cobbs@gmail.com>
2021-11-24 11:06:07 -0600

    Don't stop running tests after the first failure.

--- tests/run.sh
+++ tests/run.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+FAILED_TESTS=''
 for INPUT_FILE in *.in; do
     OUTPUT_FILE1=`echo "${INPUT_FILE}" | sed -n 's/\.in$/.out1/gp'`
     OUTPUT_FILE2=`echo "${INPUT_FILE}" | sed -n 's/\.in$/.out2/gp'`
@@ -9,20 +10,26 @@ for INPUT_FILE in *.in; do
     echo "*** testing ${INPUT_FILE}..." 1>&2
     if ! ../csvprintf -x -f "${INPUT_FILE}" | diff -u "${OUTPUT_FILE1}" -; then
         echo "*** FAILED: [1] ${INPUT_FILE}" 1>&2
-        exit 1
+        FAILED_TESTS="${FAILED_TESTS} ${INPUT_FILE}/${OUTPUT_FILE1}"
     fi
     if ! ../csvprintf -X -f "${INPUT_FILE}" | diff -u "${OUTPUT_FILE2}" -; then
         echo "*** FAILED: [2] ${INPUT_FILE}" 1>&2
-        exit 1
+        FAILED_TESTS="${FAILED_TESTS} ${INPUT_FILE}/${OUTPUT_FILE2}"
     fi
     if ! ../csvprintf -e iso-8859-1 -x -f "${INPUT_FILE}" | xsltproc ../csv.xsl - | ../csvprintf -e UTF-8 -x | diff -u "${OUTPUT_FILE1}" -; then
         echo "*** FAILED: [3] ${INPUT_FILE}" 1>&2
-        exit 1
+        FAILED_TESTS="${FAILED_TESTS} ${INPUT_FILE}/csv2xml"
     fi
     if ! ../csvprintf -ij -f "${INPUT_FILE}" | diff -u "${OUTPUT_FILE3}" -; then
         echo "*** FAILED: [3] ${INPUT_FILE}" 1>&2
-        exit 1
+        FAILED_TESTS="${FAILED_TESTS} ${INPUT_FILE}/${OUTPUT_FILE3}"
     fi
 done
-echo "*** all tests passed"
+
+if [ -z "${FAILED_TESTS}" ]; then
+    echo "*** all tests passed"
+else
+    echo "*** test(s) failed:${FAILED_TESTS}"
+    exit 1
+fi
 
