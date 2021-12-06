41cdffdd510b0832fc4d90740111c9ce6aaa2de4
Archie L. Cobbs <archie.cobbs@gmail.com>
2021-12-06 16:13:40 -0600

    Add separate test files for "-j" and "-ij" flags.

--- tests/run.sh
+++ tests/run.sh
@@ -6,7 +6,8 @@ FAILED_TESTS=''
 for INPUT_FILE in *.in; do
     OUTPUT_FILE1=`echo "${INPUT_FILE}" | sed -n 's/\.in$/.out1/gp'`
     OUTPUT_FILE2=`echo "${INPUT_FILE}" | sed -n 's/\.in$/.out2/gp'`
-    OUTPUT_FILE3=`echo "${INPUT_FILE}" | sed -n 's/\.in$/.out3/gp'`
+    OUTPUT_FILE3A=`echo "${INPUT_FILE}" | sed -n 's/\.in$/.out3a/gp'`
+    OUTPUT_FILE3B=`echo "${INPUT_FILE}" | sed -n 's/\.in$/.out3b/gp'`
     OUTPUT_FILE4=`echo "${INPUT_FILE}" | sed -n 's/\.in$/.out4/gp'`
     echo "*** testing ${INPUT_FILE}..." 1>&2
     if ! ../csvprintf -x -f "${INPUT_FILE}" | diff -u "${OUTPUT_FILE1}" -; then
@@ -18,12 +19,16 @@ for INPUT_FILE in *.in; do
         FAILED_TESTS="${FAILED_TESTS} ${INPUT_FILE}/${OUTPUT_FILE2}"
     fi
     if ! ../csvprintf -e iso-8859-1 -x -f "${INPUT_FILE}" | xsltproc ../csv.xsl - | ../csvprintf -e UTF-8 -x | diff -u "${OUTPUT_FILE1}" -; then
-        echo "*** FAILED: [3] ${INPUT_FILE}" 1>&2
+        echo "*** FAILED: [1x] ${INPUT_FILE}" 1>&2
         FAILED_TESTS="${FAILED_TESTS} ${INPUT_FILE}/csv2xml"
     fi
-    if ! ../csvprintf -ij -f "${INPUT_FILE}" | diff -u "${OUTPUT_FILE3}" -; then
-        echo "*** FAILED: [3] ${INPUT_FILE}" 1>&2
-        FAILED_TESTS="${FAILED_TESTS} ${INPUT_FILE}/${OUTPUT_FILE3}"
+    if ! ../csvprintf -j -f "${INPUT_FILE}" | diff -u "${OUTPUT_FILE3A}" -; then
+        echo "*** FAILED: [3a] ${INPUT_FILE}" 1>&2
+        FAILED_TESTS="${FAILED_TESTS} ${INPUT_FILE}/${OUTPUT_FILE3A}"
+    fi
+    if ! ../csvprintf -ij -f "${INPUT_FILE}" | diff -u "${OUTPUT_FILE3B}" -; then
+        echo "*** FAILED: [3b] ${INPUT_FILE}" 1>&2
+        FAILED_TESTS="${FAILED_TESTS} ${INPUT_FILE}/${OUTPUT_FILE3B}"
     fi
     if ! ../csvprintf -ix -f "${INPUT_FILE}" | diff -u "${OUTPUT_FILE4}" -; then
         echo "*** FAILED: [4] ${INPUT_FILE}" 1>&2
